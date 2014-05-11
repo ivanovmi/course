fedff473520a08976ce4f6b31971d46e3847bf3b
Michael <difmel@gmail.com>
2014-05-11 16:31:29 +0400

    Delete try.py

--- try.py
+++
@@ -1,9 +0,0 @@
-a = [[2,2,2],[2,2,2]]
-b = [[2,2,2],[2,2,2]]
-
-try:
-    for i,j in a:
-        for l,m in b:
-            (i!=l) and (j!=m)
-except Exception:
-    print "Error in dimension of the matrix. Dimensions of the matrices are not the same"
